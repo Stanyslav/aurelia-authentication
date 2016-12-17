62918cfa74ca51bfdfa159fb7328e53acd709b6e
gsv <you.root@gmail.com>
2016-12-18 02:47:41 +0300

    Fix client_secret Add clientSecret to config

--- dist/es2015/authFilterValueConverter.js
+++
@@ -1,7 +0,0 @@
-import { RouteConfig } from 'aurelia-router';
-
-export let AuthFilterValueConverter = class AuthFilterValueConverter {
-  toView(routes, isAuthenticated) {
-    return routes.filter(route => typeof route.config.auth !== 'boolean' || route.config.auth === isAuthenticated);
-  }
-};
